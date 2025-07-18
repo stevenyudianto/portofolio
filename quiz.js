fa984a651b27968cfea7302d02c2742baf9e8141
stevenyudianto <banggbul35@gmail.com>
2025-07-18 20:52:13 +0700

    porto

---
+++ quiz.js
@@ -0,0 +1,15 @@
+let angkaPertama=8;
+let angkaKedua=9;
+let hasil= angkaPertama*angkaKedua;
+document.writeln("Hasil Perkalian Dari" +    angkaPertama +"x" + angkaKedua +"="+ hasil)
+document.writeln("</br>");
+let hasilKedua= hasil/angkaKedua;
+document.writeln("hasil pembagian dari" + hasil +":"+ angkaKedua +"="+ hasilKedua)
+document.writeln("</br>");
+let modulus=3;
+let hasilKetiga= hasilKedua%modulus;
+document.writeln("hasil modulus dari" + hasilKedua +"%"+ modulus +"="+ hasilKetiga)
+document.writeln("</br>");
+let hasilKeempat= hasilKetiga**angkaKedua;
+document.writeln("hasil eksponensial dari" + hasilKetiga +"**"+ angkaKedua +"="+ hasilKeempat)
+
---
+++ script.js
@@ -0,0 +1,36 @@
+document.writeln("Hello World");
+document.writeln("</br>");
+document.writeln(100);
+document.writeln("</br>");
+document.writeln(100.1);
+document.writeln("</br>");
+//boolean
+document.writeln(true);
+document.writeln("</br>");
+document.writeln(false);
+//pengingat penggabungan string
+document.writeln("Nama: " + "Steven Yudianto");
+document.writeln("</br>");
+document.writeln("Mugiwara: " + "Monkey D Luffy");
+//cara untuk mendeklarasikan variabel
+var firstName="dwi"
+var firstName="cardo"
+var firstName="dandi"
+document.writeln(firstName)
+var lastName
+document.writeln("</br>")
+//Membuat Variable Langsung Dengan Value
+var angkaPertama=18;
+var angkaKedua=12;
+var angkaHasil= angkaPertama*angkaKedua;
+document.writeln("Hasil Perkalian Dari" + angkaPertama +"x" + angkaKedua +"="+ angkaHasil)
+//variable let
+document.writeln("</br>")
+let pertamaName= "steven";
+let keduaName="Yudianto"
+let fullName= pertamaName + " " + keduaName
+document.writeln(fullName)
+//variable constanta
+const fulllName = "Steven Yudianto";
+fulllName = "Satine Zaneta";
+//
